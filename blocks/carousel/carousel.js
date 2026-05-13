9242bca52bf62c77b27253493155120eef68d236
learningaemeds-vj <learningaemeds@gmail.com>
2026-05-13 18:34:21 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,8 +1,8 @@
 export default function decorate(block){
 [...block.children].forEach((row) => {
-  console.log(row);
+  console.log("row = "+row);
   [...row.children].forEach((col) => {
-    console.log(col);
+    console.log("col = "+col);
   });
 });
   
